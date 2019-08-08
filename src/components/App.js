ea1b8c0e48d909c52513af4a221853e8c12988b6
표호근 <pyohogeun@gmail.com>
2019-08-09 00:28:08 +0900

    router & route 추가

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import { HashRouter as Router, Route } from 'react-router-dom';
 import AppShell from './Appshell';
+import Home from './Home';
+import Texts from './Texts';
+import Words from './Words';
 class App extends React.Component {
     render() {
         return (
-            <AppShell></AppShell>
+            <Router>
+                <AppShell>
+                    <div>
+                        <Route exact path="/" component={Home}/>
+                        <Route exact path="/texts" component={Texts}/>
+                        <Route exact path="/words" component={Words}/>
+                    </div>
+                </AppShell>
+            </Router>
         );
     }
 }
