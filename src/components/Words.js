f7d4cdcbe8273cfea81efb4b7f3066a7033d4d90
pyo <pyohogeun@gmail.com>
2019-08-09 13:46:09 +0900

    create and delete function

--- src/components/Words.js
+++ src/components/Words.js
@@ -2,6 +2,24 @@ import React from "react";
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
 import Typography from "@material-ui/core/Typography";
+import { withStyles } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
+import Button from "@material-ui/core/Button";
+import Fab from "@material-ui/core/Fab";
+import AddIcon from "@material-ui/icons/Add";
+import Dialog from "@material-ui/core/Dialog";
+import DialogAction from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import TextField from "@material-ui/core/TextField";
+
+const styles = theme => ({
+  fab: {
+    position: "fixed",
+    bottom: "20px",
+    right: "20px"
+  }
+});
 
 const databaseURL = "https://word-cloud-736df.firebaseio.com";
 
@@ -9,7 +27,10 @@ class Words extends React.Component {
   constructor() {
     super();
     this.state = {
-      words: {}
+      words: {},
+      dialog: false,
+      word: "",
+      weight: ""
     };
   }
   _get() {
@@ -22,11 +43,61 @@ class Words extends React.Component {
       })
       .then(words => this.setState({ words: words }));
   }
-  shouldComponentUpdate(nextProps, nextState) {
-    return nextState.words != this.state.words;
+  _post(word) {
+    return fetch(`${databaseURL}/words.json`, {
+      method: "POST",
+      body: JSON.stringify(word)
+    })
+      .then(res => {
+        if (res.status != 200) {
+          throw new Error(res.statusText);
+        }
+        return res.json();
+      })
+      .then(data => {
+        let nextState = this.state.words;
+        nextState[data.name] = words;
+        this.setState({ words: nextState });
+      });
+  }
+  _delete(id) {
+      return fetch(`${databaseURL}/words/${id}.json`), {
+          method: 'DELETE'
+    }).then(res => {
+          if(res.status != 200) {
+              throw new Error(res.statusText);
+          }
+          return res.json();
+      }).then(() => {
+        let nextState = this.state.words;
+        delete nextState[id];
+        this.setState({ words: nextState });
+      });
   }
   componentDidMount() {
     this._get();
+  }
+  handleDialogToggle = () => this.setState({
+      dialog: !this.state.dialog
+  });
+  handleValueChange = (e) => {
+      let nextState = {};
+      nextState[e.target.name] = e.target.value;
+      this.setState(nextState);
+  }
+  handleSubmit = () => {
+      const word = {
+          word: this.state.word,
+          weight: this.state.weight
+      }
+      this.handleDialogToggle();
+      if(!word.word && !word.weight){
+          return;
+      }
+      this._post(word);
+  }
+  handleDelete = (id) => {
+      this._delete(id);
   }
   render() {
     return (
@@ -40,9 +111,16 @@ class Words extends React.Component {
                   <Typography color="textSecondary" gutterBottom>
                     가중치 : {word.weight}
                   </Typography>
-                  <Typography varient="h5" component="h2">
-                    {word.word}
-                  </Typography>
+                  <Grid containder>
+                      <Grid item xs={6}>
+                        <Typography varient="h5" component="h2">
+                            {word.word}
+                        </Typography>
+                      </Grid>
+                      <Grid item xs={6}>
+                          <Button variant="contained" color="primary" onClick={() => this.handleDelete(id)}>삭제</Button>
+                      </Grid>
+                  </Grid>
                 </CardContent>
               </Card>
             </div>
