f68ee9ad7f5981ade8e454b55d3aa84e4016ba00
pyo <pyohogeun@gmail.com>
2019-08-09 11:44:05 +0900

    firebase 연동 성공

--- src/components/Words.js
+++ src/components/Words.js
@@ -1,17 +1,56 @@
-import React from 'react';
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
+import React from "react";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import Typography from "@material-ui/core/Typography";
+
+const databaseURL = "https://word-cloud-736df.firebaseio.com";
 
 class Words extends React.Component {
-    render(){
-        return (
-            <Card>
+  constructor() {
+    super();
+    this.state = {
+      words: {}
+    };
+  }
+  _get() {
+    fetch(`${databaseURL}/words.json`)
+      .then(res => {
+        if (res.status != 200) {
+          throw new Error(res.statusText);
+        }
+        return res.json();
+      })
+      .then(words => this.setState({ words: words }));
+  }
+  shouldComponentUpdate(nextProps, nextState) {
+    return nextState.words != this.state.words;
+  }
+  componentDidMount() {
+    this._get();
+  }
+  render() {
+    return (
+      <div>
+        {Object.keys(this.state.words).map(id => {
+          const word = this.state.words[id];
+          return (
+            <div key={id}>
+              <Card>
                 <CardContent>
-                    Words 페이지
+                  <Typography color="textSecondary" gutterBottom>
+                    가중치 : {word.weight}
+                  </Typography>
+                  <Typography varient="h5" component="h2">
+                    {word.word}
+                  </Typography>
                 </CardContent>
-            </Card>
-        )
-    }
+              </Card>
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
 }
 
 export default Words;
